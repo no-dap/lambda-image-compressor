d170a80f8e9526262e0dda3fe28052943f06a34b
no-dap <somniholic@naver.com>
2020-06-01 17:50:16 +0900

    Modify some

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,9 @@
+import json
 import os
 from io import BytesIO
 
-from libs import boto3
-from libs.PIL import Image
+import boto3
+from PIL import Image
 
 MAX_SIZE = (1080, 810)
 BUCKET_NAME = os.getenv("BUCKET_NAME", "TODO")
@@ -10,38 +11,40 @@ AWS_REGION = os.getenv("AWS_REGION", "ap-northeast-2")
 s3_client = boto3.client('s3', region_name=AWS_REGION)
 
 
+def _post_to_s3(byte_string, size, path, name, extension):
+    img = Image.open(BytesIO(byte_string))
+
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+
+    img.thumbnail(size, Image.ANTIALIAS)
+    output = BytesIO()
+    img.save(output, format=extension, quality=100)
+    output.seek(0)
+
+    key = f'{path}/{name}.{extension}'
+    s3_client.put_object(Bucket=BUCKET_NAME, Key=key, Body=output)
+
+
 def lambda_handler(payloads, context):
     """
     Create compressed image from origin && save to S3
     :param payloads: dict
     {
-        "image": byte string,
-        "path": string, path of target image
+        "image": utf-8 encoded byte string,
+        "path": string, path of target image,
+        "name": string, name of image
     }
     :return: void
     """
-    image = payloads.get('image')
-    path = payloads.get('path')
-    origin_img_name, extension = image.name.rsplit('.', 1)
-    img = Image.open(image)
+    payloads = json.loads(payloads)
 
-    if img.mode != 'RGB':
-        img = img.convert('RGB')
-
-    img.thumbnail(MAX_SIZE, Image.ANTIALIAS)
-    output = BytesIO()
-    img.save(output, format=extension, quality=100)
-    s3_client.put_object(Bucket=BUCKET_NAME, Key=f'{path}/{origin_img_name}.{extension}', Body=output)
+    _image = payloads.get('image').encode()
+    _path = payloads.get('path')
+    _name = payloads.get('name')
+    _origin_img_name, _extension = _name.rsplit('.', 1)
+    _post_to_s3(_image, MAX_SIZE, _path, _name, _extension)
 
     for prefix, size in ('xs', (100, 100)), ('sm', (200, 200)), ('md', (400, 400)), ('lg', (800, 800)):
-        img = Image.open(image)
-
-        if img.mode != 'RGB':
-            img = img.convert('RGB')
-
-        img.thumbnail(size, Image.ANTIALIAS)
-        output = BytesIO()
-        img.save(output, format=extension, quality=100)
-
-        img_name = f'{prefix}_{origin_img_name}.{extension}'
-        s3_client.put_object(Bucket=BUCKET_NAME, Key=f'{path}/{img_name}', Body=output)
+        _name = f'{prefix}_{_origin_img_name}'
+        _post_to_s3(_image, size, _path, _name, _extension)
